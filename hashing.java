75996ac37a90a8a1793e548133fef8ce797af42e
imtiyaz98 <talktoimtiaz94@gmail.com>
2023-06-13 20:50:51 +0530

    first commit

---
+++ hashing.java
@@ -0,0 +1,29 @@
+package com.day10;
+
+import java.util.*;
+
+public class hashing {
+
+	public static void main(String[] args) {
+		// employee_name(value), employee_id(key)
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		
+		//Insertion
+		map.put("Sameer", 120);
+		map.put("Khalid", 220);
+		map.put("Ahmad", 330);
+		map.put("Nanhe", 440);
+		map.put("Minhaz", 559);
+		map.put("Kashif", 300);
+		
+		
+		System.out.println(map);
+		
+		
+	
+
+	}
+
+}
